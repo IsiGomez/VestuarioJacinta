200b6cf39077c0a4f5892c111e5f1666568ae5e8
Lukas Calvo <lu.calvo@duocuc.cl>
2026-09-04 04:13:16 -0400

    cambios en style.css para ajustar tamaño del contenedor de formularios, retoques en login.html e inicio en desarrollo de validacion de formulario

--- scripts/login.js
+++ scripts/login.js
@@ -20,4 +20,58 @@ document.addEventListener("DOMContentLoaded",()=>{
             registerForm.classList.toggle('oculto');
         });
     }
+
+    function mostrarError(idElemento, mensaje) {
+        const elemento = document.getElementById(idElemento);
+        if (elemento) {
+            elemento.textContent = mensaje;
+        }
+    }
+
+    function limpiarError(idElemento) {
+        const elemento = document.getElementById(idElemento);
+        if (elemento) {
+            elemento.textContent = "";
+        }
+    }
+
+    const logForm = document.getElementById("secc-log");
+    if (logForm) {
+        logForm.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            let esValido = true;
+
+            const email = document.getElementById("log-email");
+            const pass = document.getElementById("log-pass");
+
+            const valorEmail = email.value.trim();
+            if (valorEmail === ""){
+                mostrarError("error-log-email", "El correo electrónico no puede estar vacío");
+                esValido=false
+            } else if (!valorEmail.endsWith("@duoc.cl") &&
+            !valorEmail.endsWith("@profesor.duoc.cl") &&
+            !valorEmail.endsWith("@gmail.com")) {
+                mostrarError("error-log-email",
+                    "El correo electrónico debe incluir dominio (@duoc.cl, @profesor.duoc.cl o @gmail.com)");
+                esValido=false
+            }
+
+            const valorPass = pass.value.trim();
+            if (valorPass === ""){
+                mostrarError("error-log-pass", "La contraseña no puede estar vacía");
+                esValido=false
+            } else if (valorPass.length < 4){
+                mostrarError("error-log-pass", "La contraseña debe ser de al menos 4 caracteres");
+                esValido=false
+            } else if (valorPass.length > 10){
+                mostrarError("error-log-pass", "La contraseña debe ser de menos de 10 caracteres");
+                esValido=false
+            }
+
+            if (esValido) {
+                window.location.href = "/.";
+            }
+        });
+    }
+
 });
