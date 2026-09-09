4b2a1210d9e128baf9de68e33fa835fc17830cc5
AV-Alumno <AV-Alumno@duoc.acad>
2026-09-09 09:44:53 -0300

    Arreglo de bug en carrito al añadir producto

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -72,7 +72,7 @@ function renderizarProductos() {
                     <h3>${prod.nombre}</h3>
                 </a>
                 <p>$${prod.precio.toLocaleString('es-CL')}</p>
-                <button class="btn w-100 btn-principal" onclick="agregarProducto('${prod.nombre}', ${prod.precio}, '${prod.imagen}', '${prod.descripcion}', 1)">Añadir</button>
+                <button class="btn w-100 btn-principal" onclick="agregarProducto('${prod.nombre}', ${prod.precio}, '${prod.imagen}', 1)">Añadir</button>
             </div>
         `;
     });
